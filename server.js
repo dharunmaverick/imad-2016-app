ebe08e38872988b5be8ca1c75065907b39d93f00
dharunmaverick <thirupathidharun@gmail.com>
2016-10-02 14:03:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','test.html'));
+    
+});
+
 app.get('/demon',function(req,res){
     res.send('this is demon');
 });
