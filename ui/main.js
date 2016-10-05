22745f948abca4f38794920e3becb8244634ebc7
dharunmaverick <thirupathidharun@gmail.com>
2016-10-05 20:27:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,11 @@ var element=document.getElementById('sam');
 element.innerHTML='this is console page for imad done by dharun :) ';
 
 var img=document.getElementById('img')
-img.onclick=function()
-{
-    img.style.marginLeft='500px';
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+    img.onclick=function(){
+    var interval=setInterval(moveRight,10);
 };
