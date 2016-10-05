f66041ef64e0f30e3b49908460bc11167f2cdb42
dharunmaverick <thirupathidharun@gmail.com>
2016-10-05 19:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,42 +1,20 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/demon',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','test.html'));
-    
-});
-
-app.get('/demon',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','test.html'));
-    
-});
-app.get('/maverick',function(req,res){
-    res.send('this is maverick');
-});
-
-app.get('ui/style.css',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','style.css'));
-    
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+             console page of dharun!!!
+        </div>
+        <script>
+            alert('welcome demon!!!');
+        </script>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
