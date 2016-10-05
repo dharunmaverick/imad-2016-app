5c2b40794ed1d0d2440f53be07b0aaacde796bed
dharunmaverick <thirupathidharun@gmail.com>
2016-10-05 19:29:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,12 +14,16 @@ app.get('/demon',function(req,res){
     
 });
 
+app.get('/demon',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','test.html'));
+    
+});
 app.get('/maverick',function(req,res){
     res.send('this is maverick');
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
