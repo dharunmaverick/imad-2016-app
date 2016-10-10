f56e42b5a66006e714e29dcba0503e543d56b5ab
dharunmaverick <thirupathidharun@gmail.com>
2016-10-10 12:30:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,17 @@
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById('counters');
-    span.innerHTML=counter.toString();
-};
+    var request=new XMLhttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('counters');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://dharunmaverick.imad.hasura-app.io/count',true);
+    request.send(null);
+            };
+ 
 
