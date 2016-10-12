d8c4cf4f6d5e281a9e2acf7ec3e136dab61f3a92
dharunmaverick <thirupathidharun@gmail.com>
2016-10-12 20:55:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.get('/count',function(req,res){
 });
 
 var names=[];
-app.get('/sub',function(req,res){
-    var name=req.query.name;
-    names.push(name);
+app.get('/subs',function(req,res){
+    var para=req.query.para;
+    names.push(para);
     res.send(JSON.stringify(names));
 });
 
