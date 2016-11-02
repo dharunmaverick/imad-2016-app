0751ef2eab6585214d5401888f2f31dd0aa05afe
dharunmaverick <thirupathidharun@gmail.com>
2016-11-02 20:33:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,17 @@ app.get('/sub',function(req,res){
     names.push(name);
     res.send(JSON.stringify(names));
 });
-
+app.get('/ui/:id/comments', function (req, res)
+{
+  //url type: ui/3/comments?comment=... here id = 3
+  var id=req.params.id;
+  var id_no=parseInt(id,10);//convertin id containg string type  value to int type decimal value
+  var comment=req.query.comment;
+  if (comment!=="")
+    {list[id_no].push(comment);}
+  //returning only the row containing the comments from current page as JSON string represntation of that row,stored in a 2-D array
+  res.send(JSON.stringify(list[id_no]));
+});
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate[articleName]);
