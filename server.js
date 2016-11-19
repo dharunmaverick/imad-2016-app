cd8fcd964de22d56332c160c47fa1949db52f92d
dharunmaverick <thirupathidharun@gmail.com>
2016-11-19 14:54:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/sub',function(req,res){
     res.send(JSON.stringify(names));
 });
 
-app.get('/subs',function(req,res){
+app.get('/subcmt',function(req,res){
     var comment=req.params.comment;
     res.send(createTemplate[comment]);
     
